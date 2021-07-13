d09001e43be694d87f570c09574e4ded0aedb8b3
felipeqq <uluizfelipe@gmail.com>
2021-07-12 22:24:55 -0300

    forms click

--- utils/service.js
+++ utils/service.js
@@ -1,23 +1,47 @@
-async function postData(url = '', data = {}) {
-    console.log(JSON.stringify(data))
-    const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    // mode: 'cors', // no-cors, *cors, same-origin
+async function postData(url = '', data = {},) {
+  const response = await fetch(url, {
+    method: 'POST', 
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-}   
-  
-  postData('https://api.dev.fretec.app/api/email-contact', { 
-      "fullName": "Luiz Felipe", 
-      "state": "RN", "city": "Natal", 
-      "email": "uluizfelipe@gmail.com", 
-      "phone": "32239891", 
-      "description": "Olá" })
+    body: JSON.stringify(data)
+  });
+}
+    
+const btn = document.querySelector("#button-contact");
 
-    .then(data => {
-      console.log(data, 'email enviado');
-    }).catch(error => console.log(error, 'deu ruim'));
+btn.addEventListener("click", function(e){
+  e.preventDefault();
+
+  const name = document.querySelector("#name-contact").value;
+  const email = document.querySelector("#email-contact").value;
+  const phone = document.querySelector("#phone-contact").value;
+  const state = document.querySelector("#estado").value;
+  const city = document.querySelector("#cidade2").value;
+  const text = document.querySelector("#text-contact").value;
+
+  const data = {"fullName": name, email, phone, state, city, "description": text }
+  console.log(data)
+
+  postData('https://api.dev.fretec.app/api/email-contact', data).then(respose => console.log('email enviado'))
+})
+
+const btnLicensed = document.querySelector("#button-licensed");
+
+btnLicensed.addEventListener("click", function(e){
+  e.preventDefault();
+
+  const firstName = document.querySelector("#first-name-licensed").value;
+  const lastName = document.querySelector("#last-name-licensed").value;
+  const email = document.querySelector("#email-licensed").value;
+  const phone = document.querySelector("#phone-licensed").value;
+  const cellphone = document.querySelector("#cellphone-licensed").value;
+  const state = document.querySelector("#estado").value;
+  const city = document.querySelector("#cidade").value;
+  const text = document.querySelector("#text-licensed").value;
+
+  const data2 = {firstName, lastName, email, phone, cellphone, state, city, "description": text }
+
+  postData('https://api.dev.fretec.app/api/email-licensed', data2).then(respose => console.log('email enviado'))
+})
