a267456f514b24f4233ceffed4171f488fb6b33a
felipeqq <uluizfelipe@gmail.com>
2021-07-13 20:12:21 -0300

    folder structure and states js

--- src/js/service.js
+++ src/js/service.js
@@ -24,7 +24,9 @@ formContact.addEventListener("submit", function(e){
 
   const data = {"fullName": name, email, phone, state, city, "description": text }
 
-  postData('https://api.dev.fretec.app/api/email-contact', data).then(respose => console.log('email enviado'))
+  postData('https://api.dev.fretec.app/api/email-contact', data).then(response => console.log('email enviado'))
+
+  hiddenOrBlockContact(document.querySelector("#contact"), document.querySelector("#send-contact"));
 })
 
 const formLicensed = document.querySelector('#be-licensed')
@@ -45,4 +47,16 @@ formLicensed.addEventListener("submit", function(e){
   const data2 = {firstName, lastName, email, phone, cellphone, state, city, "description": text }
 
   postData('https://api.dev.fretec.app/api/email-licensed', data2).then(respose => console.log('email enviado'))
+
+  hiddenOrBlock(document.querySelector("#licensed"), document.querySelector("#send"));
 })
+
+function hiddenOrBlock(div1, div2) {
+  div1.style.display = "none";
+  div2.style.cssText = "display: flex;" + "width: 100%;" + "height: 100%;" + "margin: 20px auto 20px" + "flex-direction: column;" + "align-items: center;";
+}
+
+function hiddenOrBlockContact(div1, div2) {
+  div1.style.display = "none";
+  div2.style.cssText = "display: flex;" + "width: 100%;" + "flex-direction: column;" + "align-items: center;";
+}
